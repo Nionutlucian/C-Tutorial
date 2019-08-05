2fb3631ce86a1a8c44b031932a6aea5108dd21a4
Nionutlucian <nicolescuionutluci@gmail.com>
2019-08-05 15:13:38 +0300

    commit#1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ namespace FirstApp
 {
     public class Program
     {
+        public static int x = 10;
+        public static double y = 10.5;
+        public static bool z = true;
+        public static char c = 'A';
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+            Console.WriteLine("z: " + z);
+            Console.WriteLine("c: " + c);
         }
     }
 }
